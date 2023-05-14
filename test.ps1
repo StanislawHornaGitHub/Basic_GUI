57af3022e5c96c1e6379a146a61efa533294e427
stanislawhorna <stanislawhorna@outlook.com>
2023-05-14 10:46:54 +0200

    deleted:    test.ps1

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$TimerNew = [System.Diagnostics.Stopwatch]::StartNew()
-Remove-Item -Path C:\Temp\Teststatus -Force -Confirm:$false
-new-item -Type File -Path C:\Temp\Teststatus1
-$TimerNew.Stop()
-
-$TimerRename = [System.Diagnostics.Stopwatch]::StartNew()
-Rename-Item -Path C:\Temp\Teststatus -NewName "teststatus1"
-$TimerRename.Stop()
